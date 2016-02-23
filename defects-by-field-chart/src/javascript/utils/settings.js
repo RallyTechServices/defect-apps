a9e41671f608a84107379942818f34787245f79f
kcorkan <kcorkan@rallydev.com>
2016-02-23 15:29:05 -0700

    added feedback per first demo

--- defects-by-field-chart/src/javascript/utils/settings.js
+++ defects-by-field-chart/src/javascript/utils/settings.js
@@ -1,10 +1,18 @@
 Ext.define('Rally.technicalservices.DefectsByFieldSettings',{
     singleton: true,
 
-    getFields: function(settings){
-        var labelWidth = 100,
-            width = 250;
-        console.log('settings',settings);
+    getFields: function(settings, states){
+        var labelWidth = 150,
+            width = 300;
+        
+
+        var stateOptions = _.map(states, function(s){
+            console.log('state', s,Ext.Array.contains(settings.allowedStates, s));
+            var checked = Ext.Array.contains(settings.allowedStates, s);
+            return { boxLabel: s, name: 'allowedStates', inputValue: s, checked: checked };
+        });
+
+
         return [{
             xtype: 'tsfieldoptionscombobox',
             name: 'bucketField',
@@ -15,17 +23,15 @@ Ext.define('Rally.technicalservices.DefectsByFieldSettings',{
             model: settings.modelName,
             allowNoEntry: false
         },{
-            xtype: 'rallyfieldvaluecombobox',
-            name: 'allowedStates',
-            fieldLabel: 'Include Defect States',
+            xtype: 'checkboxgroup',
+            fieldLabel: 'Include States',
             labelWidth: labelWidth,
             width: width,
             labelAlign: 'right',
-            multiSelect: true,
-            model: settings.modelName,
-            value: settings.allowedStates,
-            field: 'State'
-        },{
+            columns: 1,
+            vertical: true,
+            items: stateOptions
+        }, {
             xtype: 'tsfieldoptionscombobox',
             name: 'stackField',
             fieldLabel: 'Stacks',
@@ -36,6 +42,41 @@ Ext.define('Rally.technicalservices.DefectsByFieldSettings',{
             allowNoEntry: true,
             noEntryText: '-- No Stacks --',
             noEntryValue: null
+        },{
+            xtype: 'rallycheckboxfield',
+            fieldLabel: 'Include empty categories',
+            labelWidth: labelWidth,
+            width: width,
+            labelAlign: 'right',
+            name: 'showNoDataCategories'
+        },{
+            xtype: 'textarea',
+            fieldLabel: 'Query',
+            name: 'query',
+            anchor: '100%',
+            cls: 'query-field',
+            labelAlign: 'right',
+            labelWidth: labelWidth,
+            margin: '0 70 0 0',
+            plugins: [
+                {
+                    ptype: 'rallyhelpfield',
+                    helpId: 194
+                },
+                'rallyfieldvalidationui'
+            ],
+            validateOnBlur: false,
+            validateOnChange: false,
+            validator: function(value) {
+                try {
+                    if (value) {
+                        Rally.data.wsapi.Filter.fromQueryString(value);
+                    }
+                    return true;
+                } catch (e) {
+                    return e.message;
+                }
+            }
         }];
     }
 });
